74bfbf0faf83b19c3841edb61f494eb139e44a16
UdaraDharmarathna <udara.thilinda1@gmail.com>
2021-05-14 06:22:14 +0530

    Delete Server directory

--- Server/api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'devcloud_lead_test');
-define('DB_USER', 'devcloud_lead_test');
-define('DB_PASSWORD', 'V.@CyTc*g8qx');
--- Server/api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require 'config.php';
-require 'database.php';
-require 'countdown.php';
-require 'countdownDAO.php';
-
-countdownDAO::setCountdown();
