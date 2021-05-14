cc4fc1619f04d10d2818876248c45f6ebd17a830
UdaraDharmarathna <udara.thilinda1@gmail.com>
2021-05-14 07:19:14 +0530

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'devcloud_lead_test');
-define('DB_USER', 'devcloud_lead_test');
-define('DB_PASSWORD', 'V.@CyTc*g8qx');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
